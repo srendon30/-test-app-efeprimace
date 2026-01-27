90b50d4f25c2b567f13c7cafe1095de322326706
srendon30 <srendon@controldiseno.com>
2026-01-27 09:23:35 -0500

    Updates

---
+++ assets/ProfilePicture-CYrrUQgx.js
@@ -0,0 +1,1 @@
+import a from"./ImageProfile-BJ0up8yj.js";import{J as t,g as r,o}from"./index-BHpOxlpQ.js";const m={__name:"ProfilePicture",setup(s){const e=t({margin:"auto auto",display:"flex"});return(c,i)=>(o(),r(a,{alt:"Profile",styles:e,imageName:"dr.andrescanchica.png",width:"150",height:"0"},null,8,["styles"]))}};export{m as default};
